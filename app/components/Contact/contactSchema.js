25f643e6112e19ff74a8b4527b5de675201ad74a
Omar Elbastawesy <omarelbastawesy2@gmail.com>
2025-12-24 22:17:47 +0200

    update the languages json file

--- app/components/Contact/contactSchema.js
+++ app/components/Contact/contactSchema.js
@@ -3,6 +3,6 @@ import { z } from "zod";
 export const contactSchema = z.object({
   name: z.string().min(3),
   email: z.string().email(),
-  phone: z.string().min(11),
+  phone: z.string().regex(/^\d+$/).min(11),
   message: z.string().optional(),
 });
